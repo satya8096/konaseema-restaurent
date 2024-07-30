f2e18470e5ff475d00573e4c5cc448c2d60ae3d1
satya8096 <21MH5A0326@acoe.edu.in>
2024-07-30 10:42:19 +0530

    first commit

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState, useMemo } from "react";
 
 const Order = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -66,11 +66,9 @@ const Order = () => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredOrders =
-    () =>
-      orders.filter((item) =>
-        item.name.toLowerCase().includes(searchTerm.toLowerCase())
-      )
+  const filteredOrders = orders.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <>
       <div className="order-main-container d-flex align-items-center  flex-column gap-2">
@@ -95,18 +93,24 @@ const Order = () => {
                 <th>Price</th>
                 <th>Delivery Status</th>
               </tr>
-              {orders.filter((item) => {
+              {orders
+                .filter((item) => {
                   return (
                     item.deliveryStatus === "Preparing" ||
                     item.deliveryStatus === "Out for Delivery"
                   );
-                }).map((item, index) => {
+                })
+                .map((item, index) => {
                   return (
                     <tr key={index}>
                       <td>{index + 1}</td>
                       <td>order458537689386</td>
                       <td>
-                        <img src="https://img.freepik.com/free-photo/top-view-arrangement-with-delicious-pakistan-meal_23-2148821534.jpg?t=st=1722078312~exp=1722081912~hmac=9ced514a886fd33459779357e602d1fe8baec63036ac7d4525a4c56a4e59913e&w=826" alt="item img" style={{width:"4rem"}}/>
+                        <img
+                          src="https://img.freepik.com/free-photo/top-view-arrangement-with-delicious-pakistan-meal_23-2148821534.jpg?t=st=1722078312~exp=1722081912~hmac=9ced514a886fd33459779357e602d1fe8baec63036ac7d4525a4c56a4e59913e&w=826"
+                          alt="item img"
+                          style={{ width: "4rem" }}
+                        />
                       </td>
                       <td>{item.name}</td>
                       <td>Rs. {item.price} /-</td>
